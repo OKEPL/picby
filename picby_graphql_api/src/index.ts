3efb0e103272800281ad5e2b188bd1cfa357985b
wojtek <wojciech.bilicki@gmail.com>
2020-01-11 15:39:55 +0100

    authorisation_login_flow fix PR

--- picby_graphql_api/src/index.ts
+++ picby_graphql_api/src/index.ts
@@ -10,6 +10,14 @@ import { MeResolver } from "./modules/user/Me";
 import { RegisterResolver } from './modules/user/Register';
 import { redis } from "./redis";
 
+
+const COOKIE_MAX_AGE_LIMIT = 1000*60*60*24*7*36;
+const AUTH_COOKIE_NAME = "qid";
+// TODO: we should find a way to store it secretly somewhere
+const AUTH_COOKIE_SECRET = "asdasda";
+const DEFAULT_PORT = 4000;
+
+
 (async () => {
   const app = express();
 
@@ -39,21 +47,21 @@ import { redis } from "./redis";
       store: new RedisStore({
         client: redis as any
       }),
-      name: 'qid',
-      secret: 'asdasda',
+      name: AUTH_COOKIE_NAME,
+      secret: AUTH_COOKIE_SECRET,
       resave: false,
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        maxAge: 1000*60*60*24*7*365 //7 years
+        maxAge:  COOKIE_MAX_AGE_LIMIT//7 years
 
       }
     })
   )
 
   apolloServer.applyMiddleware({ app, cors: false });
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || DEFAULT_PORT;
   app.listen(port, () => {
     console.log(`server started at http://localhost:${port}/graphql`);
   });
