34c86fdd0094bcc2608ad1973bec395cf83af1be
Wojtek Bilicki <wojciech.bilicki@oke.pl>
2020-01-16 13:53:25 +0100

    Add test environment #16 pr fix 2

--- picby_graphql_api/src/modules/user/me/Me.test.ts
+++ picby_graphql_api/src/modules/user/me/Me.test.ts
@@ -33,7 +33,6 @@ query {
 }
 `
 
-<<<<<<< HEAD
 describe('Me Resolver', () => {
   it('should retrieve current user ', async () => {
 
@@ -41,20 +40,10 @@ describe('Me Resolver', () => {
     const {email, id} = await User.create({
       email: internet.email(),
       password: internet.password(8)
-=======
-describe('Me', () => {
-  it('get user', async () => {
-
-
-    const user = await User.create({
-      email: faker.internet.email(),
-      password: faker.internet.password(8)
->>>>>>> master
     }).save();
 
    const response =  await gCall({
       source: meQuery,
-<<<<<<< HEAD
       userId: id
     })
 
@@ -64,28 +53,12 @@ describe('Me', () => {
         me: {
           id: `${id}`,
           email: email
-=======
-      userId: user.id
-    })
-
-
-    console.log(response);
-    expect(response).toMatchObject({
-      data: {
-        me: {
-          id: `${user.id}`,
-          email: user.email
->>>>>>> master
         }
       }
     })
   })
 
-<<<<<<< HEAD
   it("return null if there's no userId in context", async () => {
-=======
-  it("return null", async () => {
->>>>>>> master
     const response = await gCall({
       source: meQuery
     })
